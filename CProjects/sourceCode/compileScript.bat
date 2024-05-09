092faaf0e0370a1ba158cc7d7ca18093d1d56c9f
Sam_Frederickson <tntfredcode@gmail.com>
2024-05-08 21:06:28 -0500

    The start of a calculator in C & a lil scripting

--- CProjects/sourceCode/compileScript.bat
+++ CProjects/sourceCode/compileScript.bat
@@ -1,3 +1,7 @@
 REM @echo off
 
 gcc -o C:\Users\Sam\Documents\codingProjects\CProjects\complied\%1 %2
+
+del *~
+
+del *.un~
---
+++ CProjects/sourceCode/simpleAddition.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+int firstOperand;
+int operator;
+int secondOperand:
+
+  printf("Enter a number: ");
+  scanf("%d", firstOperand);
+
+  printf("Enter an Operand: ");
+  scanf("%d", operator);
+
+  printf("Enter ")
+
+}
