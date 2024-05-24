6dc765f0e3a028a2aaffbaad343df8ceb38160d6
Sam_Frederickson <tntfredcode@gmail.com>
2024-05-23 21:36:29 -0500

    The addition of messing with arrays and the start of an ecryption

---
+++ CProjects/sourceCode/messingWithArrays.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int  testArray();
+
+void feedArray(int arr[]);
+
+void main() {
+  feedArray();
+  printf("%d"feedArray());
+}
+
+int feedArray(int arr[]) {
+  for (int i = 1; i <= 5; i++) {
+    int newArray[] = int arr[i];
+    
+  }
+}
